d90a9890ba7770049e170c1dced39ed3d1ae7f12
pericles842 <slouis482@gmail.com>
2022-04-05 09:50:21 -0400

    master

--- models/main.js
+++ models/main.js
@@ -22,3 +22,16 @@ function divAuto_two(){
      clic_two = 1;
     }   
  }
+  let boton_input_two = document.getElementById('boton_input_two')
+
+  boton_input_two.addEventListener('click',record)
+
+ function record() {
+   let pasward = document.getElementById('pasward_input').value
+   let tex = document.getElementById('tex_input').value
+   let help = document.getElementById('help_input').value
+   let word_two = document.getElementById('word_two')
+    
+   word.innerHTML = help
+   word_two.innerHTML = tex
+ }
